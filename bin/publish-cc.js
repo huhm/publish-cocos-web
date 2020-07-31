26913ff33da35ef996033da0582358c8a896df7b
huhuimei <huhuimei2008@qq.com>
2020-07-31 10:53:01 +0800

    fix(bin): fix commander error

--- bin/publish-cc.js
+++ bin/publish-cc.js
@@ -33,36 +33,25 @@ program
   .version(pkg.version, '-V, --version');
 program
   .usage('[options]')
-  .command('exec')
-  // 选择名 选项描述 默认值
-  // 选项 可以带有一个参数 可以通过 program.copy 获取该选项信息
-  // 如果没有参数 该值为 true
+  .description('deploy the build folder to oss')
+  //.command('exec')
   .option('-c, --config <source>', 'specify the config file(default setting is ./.publish-cc.js)')
-  .requiredOption('-v, --ver <versionFolderName>', `version folder name，if not defined use the version of the project's package.json:`,defaultVersionFolder);
-  
-program.command('init').usage('init [options]')
+  .requiredOption('-v, --ver <versionFolderName>', `version folder name，if not defined use the version of the project's package.json:`,defaultVersionFolder)
+  .action((program)=>{
+    const options=program.opts();
+    const { config, ver } = options;
+    publisherUtils.doPublish(config,ver)
+  })
+  program.command('init')
+  .usage('init [options]')
   .description('init config file')
   .usage('init [options]')
-  .option('-f, --force', '',true) 
-
-program.parse(process.argv);
-function resolve(program) {
-  const options=program.opts();
-  const args=program.args; 
-  // return;
-
-  if(args[0]==='init'){
+  .option('-f, --force', '',true)
+  .action((program)=>{
+    const options=program.opts();
     const {force } = options; 
     // create config file
     initObj.init(force);
-  }else{
-    const { config, ver } = options;
-   
-    // set dir
-    // upload to oss
-    publisherUtils.doPublish(config,ver)
-  }
-}
+  });
 
-resolve(program);
- 
+program.parse(process.argv);
